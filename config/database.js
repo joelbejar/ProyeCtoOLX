14e47b737a9cc2c23e6dbac894038d47b78db9b6
joelbe1993 <joel.bejar1@unmsm.edu.pe>
2017-04-29 11:53:54 -0500

    commit 2

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports={
+	MONGO_URL_LOCAL:'',
+	MONGO_URL_REMOTE:'mongodb://kevin:kevin@ds039010.mlab.com:39010/olx1'
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+
+//Core 
+const express=require('express');
+const mongoose=require('mongoose');
+
+//Modules application
+	//Config
+const database=require('./config/database');
+
+const app=express();
+
+mongoose.connect(database.MONGO_URL_REMOTE);
+
+
+//Endpoints
+
+app.get('/api/:pagina',(req,res)=>{
+	const pagina=req.params.pagina;
+	res.status(200).send(pagina);
+});
+
+app.listen(3000);
+
